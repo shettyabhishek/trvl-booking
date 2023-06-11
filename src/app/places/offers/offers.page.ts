ce1d1f290c1dfec0169f63fe2519a0171581d335
Abhishek <shettyabhishek829@yahoo.in>
2023-06-11 12:04:14 +0530

    Changes for offerings

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+//importing the service
+import { PlacesService } from '../places.service';
+import { Place } from '../places.model';
+
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
 })
+
 export class OffersPage implements OnInit {
+  public allOffers: Place[] | undefined;
 
-  constructor() { }
+  constructor(private _placesServ: PlacesService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewWillEnter(){
+    this.allOffers = this._placesServ.getOffers();
   }
-
 }
